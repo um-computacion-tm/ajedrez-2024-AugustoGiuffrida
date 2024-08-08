a847ac00d68ad5aab6b5864b4908092ced8da5f0
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-08-08 00:20:25 -0300

    aesthetic improvements to the board

--- game/board.py
+++ game/board.py
@@ -1,31 +1,35 @@
 class Board:
     def __init__(self):
-        # Llama al método create_board al crear una instancia de Tablero y guarda el resultado en self.tablero
         self.board = self.create_board()
 
     def create_board(self):
-        board = []  #Inicializa una lista vacía que representará el tablero
-        
-        for row in range(8): #Recorre 8 filas
-
-            board.append([]) # Añade una lista vacía para cada fila
-
-            for column in range(8):
-                if (row + column) % 2 == 0: #Alterna entre espacios claros (" ") y casillas oscuras ("#")
+        board = []
+        for row in range(8):
+            board.append([])
+            for col in range(8):
+                if (row + col) % 2 == 0:
                     board[row].append(" ")
                 else:
                     board[row].append("*")
-        
-        return board #Devuelve el tablero completo
+        return board
 
     def show_board(self):
-        output = "  a b c d e f g h\n"
-        # Recorre cada fila del tablero con su índice
+        output = "     a     b     c     d     e     f     g     h\n"
+        output += "  ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐\n"
+        
         for i, row in enumerate(self.board):
-            output += f"{8 - i} " + " ".join(row) + f" {8 - i}\n"
-        output += "  a b c d e f g h"
+            output += f"{8 - i} │"
+            for square in row:
+                output += f"  {square}  │"
+            output += f" {8 - i}\n"
+            if i < 7:
+                output += "  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤\n"
+            else:
+                output += "  └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘\n"
+        
+        output += "     a     b     c     d     e     f     g     h\n"
         return output
 
 if __name__ == "__main__":
-    board = Board()  # Crea una instancia de Board
-    print(board.show_board())  # Imprime la salida del método show_board en la consola
+    board = Board()
+    print(board.show_board())
