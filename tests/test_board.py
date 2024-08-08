6a7fd709baed50a95e72051e505cedb3203e9b58
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-08-08 16:07:37 -0300

    Fixed board tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,31 +1,29 @@
 import unittest
-import sys
-import os
 
-# Asegúrate de que la ruta del directorio del proyecto esté en sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from game.board import Board
+from game.cell import Cell
 
 class TestBoard(unittest.TestCase):
 
+
     def test_create_board(self):
-        first_board = Board().create_board()  # Llama al método create_board() 
-        first_output = [
-            [" ", "*", " ", "*", " ", "*", " ", "*"],
-            ["*", " ", "*", " ", "*", " ", "*", " "],
-            [" ", "*", " ", "*", " ", "*", " ", "*"],
-            ["*", " ", "*", " ", "*", " ", "*", " "],
-            [" ", "*", " ", "*", " ", "*", " ", "*"],
-            ["*", " ", "*", " ", "*", " ", "*", " "],
-            [" ", "*", " ", "*", " ", "*", " ", "*"],
-            ["*", " ", "*", " ", "*", " ", "*", " "]
-        ]
-        self.assertEqual(first_board, first_output)
+        board = Board().create_board()
+        for row in range(8):
+            for col in range(8):
+                expected_color = "white" if (row + col) % 2 == 0 else "black"
+                cell = board[row][col]
+                # Verificamos que el objeto sea una instancia de Cell
+                self.assertIsInstance(cell, Cell)
+                # Comparamos el color esperado con el color de la celda
+                self.assertEqual(cell.color, expected_color)
+                # Comparamos el contenido de la celda
+                self.assertEqual(cell.content, " ")
+
 
     def test_show_board(self):
-        board = Board()
-        expected_output = """     a     b     c     d     e     f     g     h
+        second_board = Board().show_board()
+        second_output = """     a     b     c     d     e     f     g     h
   ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 8 │     │  *  │     │  *  │     │  *  │     │  *  │ 8
   ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
@@ -45,12 +43,12 @@ class TestBoard(unittest.TestCase):
   └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
      a     b     c     d     e     f     g     h"""
         
-        # Captura la salida del método show_board
-        output = board.show_board()
+        normalized_board = "\n".join([line.strip() for line in second_board.splitlines()])
+        normalized_output = "\n".join([line.strip() for line in second_output.splitlines()])
 
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(normalized_board, normalized_output)
 
-##python3 -m unittest discover tests
-        
+
+
+
         
