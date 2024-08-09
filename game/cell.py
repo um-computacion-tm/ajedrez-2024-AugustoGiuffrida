df8a20e8beca21c02496d086e93fd14591c24130
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-08-09 10:12:58 -0300

    Added various functions to class Cell

--- game/cell.py
+++ game/cell.py
@@ -4,8 +4,9 @@ class Cell:
             raise ValueError(f"Invalid color: {color}")
         self.color = color
         self.content = content
+        self.piece = None 
 
-    def __str__(self): #método que se utiliza para representar una instancia de la clase como una cadena de texto
+    def __str__(self): #-->método que se utiliza para representar una instancia de la clase como una cadena de texto
         return self.content# Retorna el contenido de la celda para su visualización
                            #cuando se imprime o converte una celda a una cadena de texto, devuelve el valor almacenado 
 
@@ -16,6 +17,20 @@ class Cell:
         else:
             return "*"  # Celda negra
 
-cell = Cell("black", "p")
-print(cell)
-print (cell.display_symbol())
+    def place_piece(self, piece): #-->coloca una pieza en la celda actual
+        if self.piece is not None:#si self.piece no es None, significa que hay una pieza, no se puede poner otra pieza en esa celda.
+            raise ValueError("Cell already occupied")#indica que no se puede colocar una nueva pieza porque la celda ya está ocupada
+        self.piece = piece #permite colocar la nueva pieza.
+
+    def remove_piece(self):
+        piece = self.piece #guarda la pieza que está en la celda (self.piece) en la variable piece.
+        self.piece = None  #la celda se "vacía" estableciendo self.piece a None
+        return piece
+
+    def is_occupied(self):#-->verifica si la celda ya tiene una pieza
+        return self.piece is not None #Si hay una pieza (self.piece no es None), la función devuelve True. 
+                                      #Si no hay ninguna pieza en la celda (self.piece es None), devuelve False
+
+#cell = Cell("black", "p")
+#print(cell)
+#print (cell.display_symbol())
