72416da178052780efb3b9dd2e57c481f598af98
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-08-09 10:14:17 -0300

    Add some Cell tests

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -31,6 +31,33 @@ class TestCell(unittest.TestCase):
     def test_display_symbol(self):
         self.assertEqual(Cell("black").display_symbol(), "*")
         self.assertEqual(Cell("white").display_symbol(), " ")
+
+
+    def test_place_piece(self): #Si la pieza en la celda no coincide con la que intentamos colocar         
+        cell = Cell("white")    #la prueba fallará
+        piece = "Pawn"  
+        cell.place_piece(piece)
+        self.assertEqual(cell.piece, piece)
+
+    def test_remove_piece(self): 
+        cell = Cell("black")
+        piece = "Knight"
+        cell.place_piece(piece) #El método place_piece debe asignar la pieza a la celda.
+        removed_piece = cell.remove_piece() #Se quita la pieza de la celda utilizando el método remove_piece.
+        self.assertEqual(removed_piece, piece)#El método debería devolver la pieza que estaba almacenada en la celda antes de quitarla
+        self.assertIsNone(cell.piece) #verifica que después de haber quitado la pieza, la celda no contenga ninguna pieza 
+
+    def test_is_occupied(self):
+        cell = Cell("white")                #Se verifica que la celda recién creada no esté ocupada. 
+        self.assertFalse(cell.is_occupied())#La función is_occupied debería devolver False
+        cell.place_piece("Bishop")          #Se coloca una pieza,
+        self.assertTrue(cell.is_occupied()) ##La función is_occupied debería devolver True
+
+    def test_place_piece_on_occupied_cell(self):
+        cell = Cell("black")
+        cell.place_piece("Queen")
+        with self.assertRaises(ValueError):#si se intenta colocar una segunda pieza en una celda ocupada, debería lanzar un error
+            cell.place_piece("King")       #Se intenta colocar una segunda pieza en la celda que ya está ocupada 
                                            
 if __name__ == '__main__':
     unittest.main()
