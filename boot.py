765a09da3aa97621f28f4a304bc30a5e4ced8384
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-08-12 08:09:00 -0300

    Booting scipt

--- boot.py
+++ boot.py
@@ -2,9 +2,10 @@
 
 from game.board import Board
 
-if __name__ == "__main__":
-    board = Board()
-    print(board.show_board())
 
+# Código para ejecutar la clase Board si este archivo se ejecuta como un script.
+if __name__ == "__main__":
+    board = Board()  # Crea una instancia del tablero de ajedrez.
+    print(board.show_board())  # Imprime el tablero de ajedrez en formato de texto.
 
 #python boot.py
