abddfe45fdd28d23d9167a5b5529bd6be3b4c0be
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-08-18 18:32:12 -0300

    Fix some issues

--- game/piece.py
+++ game/piece.py
@@ -1,16 +1,14 @@
-from .cell import Cell
-
 class Pieces:
     def __init__(self, color, position):
         self.__color__ = color
         self.__position__ = position
         self.__symbol__ = None
 
-    def move(self, new_position):
-        if new_position in self.valid_moves:
+    def move(self, new_position, board):
+        if new_position in self.valid_moves(board):
             self.__position__ = new_position
         else:
-            raise ValueError("Movimiento no válido.")    
+            raise ValueError("Movimiento no válido.")
 
     def valid_moves(self, board):
         raise NotImplementedError("Este método debe ser implementado en subclases.")
@@ -27,57 +25,59 @@ class Pieces:
     def get_symbol(self):
         return self.__symbol__
 
-
 class Rook(Pieces):
     def __init__(self, color, position):
         super().__init__(color, position)
 
     def valid_moves_vertical(self, board):
         moves = []
-        row, column = self.get_position() 
+        row, column = self.get_position()
 
-        #Movimientos hacia arriba (columna fija, fila decrece)
-        for i in range (row-1,-1,-1):
+        # Movimientos hacia arriba (columna fija, fila decrece)
+        for i in range(row - 1, -1, -1):
             cell = board[i][column]
-            if cell.is_occupied:
-                if cell.get_piece().get_color() != self.get_color(): #Si la pieza es de otro color
-                    moves.append((i,column))                       #el movimiento hacia esa celda es válido.
+            if cell.is_occupied():
+                if cell.get_piece() and cell.get_piece().get_color() != self.get_color():
+                    moves.append((i, column))
                 break
             moves.append((i, column))
 
-        #Movimientos hacia abajo (columna fija, fila aumenta)
-        for i in range (row+1,8):
+        # Movimientos hacia abajo (columna fija, fila aumenta)
+        for i in range(row + 1, 8):
             cell = board[i][column]
-            if cell.is_occupied:
-                if cell.get_piece().get_color() != self.get_color():
-                    moves.append((i,column))                       
+            if cell.is_occupied():
+                if cell.get_piece() and cell.get_piece().get_color() != self.get_color():
+                    moves.append((i, column))
                 break
-            moves.append((i, column))    
+            moves.append((i, column))
         return moves
 
     def valid_moves_horizontal(self, board):
         moves = []
-        row, column = self.get_position() 
+        row, column = self.get_position()
 
-        #Movimientos hacia la izquierda (fila fija, columna decrece)
-        for i in range (column-1,-1,-1):
+        # Movimientos hacia la izquierda (fila fija, columna decrece)
+        for i in range(column - 1, -1, -1):
             cell = board[row][i]
-            if cell.is_occupied:
-                if cell.get_piece().get_color() != self.get_color(): 
-                    moves.append((row,i))                       
+            if cell.is_occupied():
+                if cell.get_piece() and cell.get_piece().get_color() != self.get_color():
+                    moves.append((row, i))
                 break
             moves.append((row, i))
 
-        #Movimientos hacia la derecha (fila fija, columna aumenta)
+        # Movimientos hacia la derecha (fila fija, columna aumenta)
         for i in range(column + 1, 8):
             cell = board[row][i]
             if cell.is_occupied():
-                if cell.get_piece().get_color() != self.get_color():
+                if cell.get_piece() and cell.get_piece().get_color() != self.get_color():
                     moves.append((row, i))
                 break
-            moves.append((row, i))   
-        return moves                           
-        
+            moves.append((row, i))
+        return moves
+
+    def valid_moves(self, board):
+        return self.valid_moves_vertical(board) + self.valid_moves_horizontal(board)
+
 class Pawn(Pieces):
     def __init__(self, color, position):
         super().__init__(color, position)
@@ -85,18 +85,16 @@ class Pawn(Pieces):
     def valid_moves(self, board):
         moves = []
         row, col = self.get_position()
-        
-        #Blancas suben, negras bajan
-        if self.get_color() == "white": 
-            direction = -1 
-        else:
-            direction = 1  
 
-        #Movimiento hacia adelante
-        if board[row + direction][col].get_piece() is None:  #verifica que celda en la fila delante del peón (en la misma columna) está vacía
+        # Blancas suben, negras bajan
+        direction = -1 if self.get_color() == "white" else 1
+
+        # Movimiento hacia adelante
+        if board[row + direction][col].get_piece() is None:
             moves.append((row + direction, col))
 
-            #Primer movimiento doble
+            # Primer movimiento doble
             if (self.get_color() == "white" and row == 6) or (self.get_color() == "black" and row == 1):
                 if board[row + 2 * direction][col].get_piece() is None:
                     moves.append((row + 2 * direction, col))
+        return moves
--- tests/test_piece.py
+++ tests/test_piece.py
@@ -1,7 +1,13 @@
 import unittest
 from game.piece import Pieces, Rook, Pawn
+from game.cell import Cell
 
 class TestPieces(unittest.TestCase):
+
+    def setUp(self):
+        self.board = [[Cell("white", (i, j)) for j in range(8)] for i in range(8)]
+        self.rook = Rook("white", (4, 4))
+        self.pawn = Pawn("white", (6, 0))
 
     def test_piece_initialization(self):
         piece = Pieces("white", (0, 0))
@@ -16,7 +22,33 @@ class TestPieces(unittest.TestCase):
     def test_valid_moves_not_implemented(self):
         piece = Pieces("white", (0, 0))
         with self.assertRaises(NotImplementedError):
-            piece.valid_moves(None)
+            piece.valid_moves(self.board)
+
+    def test_rook_valid_moves_vertical(self):
+        moves = self.rook.valid_moves_vertical(self.board)
+        expected_moves = [(3, 4), (2, 4), (1, 4), (0, 4), (5, 4), (6, 4), (7, 4)]
+        self.assertEqual(moves, expected_moves)
+
+    def test_rook_valid_moves_horizontal(self):
+        moves = self.rook.valid_moves_horizontal(self.board)
+        expected_moves = [(4, 3), (4, 2), (4, 1), (4, 0), (4, 5), (4, 6), (4, 7)]
+        self.assertEqual(moves, expected_moves)
+
+    def test_pawn_valid_moves_initial_position(self):
+        moves = self.pawn.valid_moves(self.board)
+        expected_moves = [(5, 0), (4, 0)]
+        self.assertEqual(moves, expected_moves)
+
+    def test_pawn_valid_moves_after_move(self):
+        self.pawn.move((5, 0), self.board)
+        moves = self.pawn.valid_moves(self.board)
+        expected_moves = [(4, 0)]
+        self.assertEqual(moves, expected_moves)
+
+    def test_pawn_cannot_move_forward_if_blocked(self):
+        self.board[5][0].place_piece(Pawn("black", (5, 0)))
+        moves = self.pawn.valid_moves(self.board)
+        self.assertEqual(moves, [])
 
 if __name__ == '__main__':
     unittest.main()
