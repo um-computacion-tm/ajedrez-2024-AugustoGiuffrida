d76b28544a1c71d59c96a9d2af698e3fcb40d40b
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-08-21 19:07:00 -0300

    Fix some issues

--- game/piece.py
+++ game/piece.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8') 
+
 class Pieces:
     def __init__(self, color, position):
         self.__color__ = color
@@ -9,9 +12,6 @@ class Pieces:
             self.__position__ = new_position
         else:
             raise ValueError("Movimiento no válido.")
-
-    def valid_moves(self, board):
-        raise NotImplementedError("Este método debe ser implementado en subclases.")
 
     def __repr__(self):
         return self.__symbol__ if self.__symbol__ is not None else ""
@@ -28,8 +28,67 @@ class Pieces:
 class Rook(Pieces):
     def __init__(self, color, position):
         super().__init__(color, position)
-
+    
+    def __repr__(self):
+        return "♖" if self.get_color() == "white" else "♜"
 
 class Pawn(Pieces):
     def __init__(self, color, position):
         super().__init__(color, position)
+         
+    def __repr__(self):
+        return "♙" if self.get_color() == "white" else "♟"    
+
+class Bishop(Pieces):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+    
+    def __repr__(self):
+        return "♗" if self.get_color() == "white" else "♝"
+
+class Knight(Pieces):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+    
+    def __repr__(self):
+        return "♘" if self.get_color() == "white" else "♞"
+
+class King(Pieces):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+    
+    def __repr__(self):
+        return "♔" if self.get_color() == "white" else "♚"
+
+class Queen(Pieces):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+    
+    def __repr__(self):
+        return "♕" if self.get_color() == "white" else "♛"
+
+       
+rook_white = Rook("white", "a1")
+rook_black = Rook("black", "h8")
+
+print(rook_white)  
+print(rook_black)  
+
+# chess_pieces2 = {
+#     "white": {
+#         "king": "♔",
+#         "queen": "♕",
+#         "rook": "♖",
+#         "bishop": "♗",
+#         "knight": "♘",
+#         "pawn": "♙"
+#     },
+#     "black": {
+#         "king": "♚",
+#         "queen": "♛",
+#         "rook": "♜",
+#         "bishop": "♝",
+#         "knight": "♞",
+#         "pawn": "♟"
+#     }
+# }
