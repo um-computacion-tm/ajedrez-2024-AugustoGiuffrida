fb75f58fbe3802434b7ebd0cd0d0e0e33f64b94a
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-08-28 20:49:21 -0300

    Improved maintainability

--- game/piece.py
+++ game/piece.py
@@ -5,16 +5,9 @@ class Pieces:
     def __init__(self, color, position):
         self.__color__ = color
         self.__position__ = position
-        self.__symbol__ = None
-
-    def move(self, new_position, board):
-        if new_position in self.valid_moves(board):
-            self.__position__ = new_position
-        else:
-            raise ValueError("Movimiento no válido.")
 
     def __repr__(self):
-        return self.__symbol__ if self.__symbol__ is not None else ""
+        return self.white_repr if self.__color__ == "white" else self.black_repr
 
     def get_color(self):
         return self.__color__
@@ -26,46 +19,30 @@ class Pieces:
         return self.__symbol__
 
 class Rook(Pieces):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-    
-    def __repr__(self):
-        return "♖" if self.get_color() == "white" else "♜"
+    white_repr = "♖"
+    black_repr = "♜"
 
 class Pawn(Pieces):
-    def __init__(self, color, position):
-        super().__init__(color, position)
+    white_repr = "♙"
+    black_repr = "♟"
          
-    def __repr__(self):
-        return "♙" if self.get_color() == "white" else "♟"    
-
-class Bishop(Pieces):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-    
-    def __repr__(self):
-        return "♗" if self.get_color() == "white" else "♝"
+class Bishop(Pieces):  
+    white_repr = "♗"
+    black_repr = "♝"  
 
 class Knight(Pieces):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-    
-    def __repr__(self):
-        return "♘" if self.get_color() == "white" else "♞"
+    white_repr = "♘"
+    black_repr = "♞"
 
 class King(Pieces):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-    
-    def __repr__(self):
-        return "♔" if self.get_color() == "white" else "♚"
+    white_repr = "♔"
+    black_repr = "♚"
+
 
 class Queen(Pieces):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-    
-    def __repr__(self):
-        return "♕" if self.get_color() == "white" else "♛"
+    white_repr = "♕"
+    black_repr = "♛"
+
 
 # chess_pieces2 = {
 #     "white": {
