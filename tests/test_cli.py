b0b5a6194803ad87b392c5970c9b41d7a6efc97e
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-09-11 22:51:13 -0300

    Added some cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,7 @@ from game.menu import Menu
 
 class TestCli(unittest.TestCase):
 
-    @patch('your_module.Menu')
+    @patch('game.menu.Menu') 
     def test_main(self, MockMenu):
         # Configurar el mock para Menu
         mock_menu = MockMenu.return_value
@@ -14,12 +14,12 @@ class TestCli(unittest.TestCase):
         cli = Cli()
         cli.main()
         # Verificar que Menu fue creado con 'cli'
-        MockMenu.assert_called_once_with(cli)
+        MockMenu.assert_called_once_with(cli)       
         # Verificar que show_start_menu fue llamado
         mock_menu.show_start_menu.assert_called_once()
 
-    @patch('module.Chess')
-    @patch('module.Cli.play')
+    @patch('game.chess.Chess')  
+    @patch('game.cli.Cli.play')
     def test_start_game(self, MockPlay, MockChess):
         mock_chess = MockChess.return_value
         mock_chess.is_playing.side_effect = [True, True, False]  # Simula dos vueltas del bucle
@@ -29,7 +29,6 @@ class TestCli(unittest.TestCase):
         MockChess.assert_called_once()
         # Verificar que play se llama mientras is_playing devuelve True
         self.assertEqual(MockPlay.call_count, 2)
-
 
 
     @patch('builtins.input', side_effect=['1', '1', '2', '2'],)
