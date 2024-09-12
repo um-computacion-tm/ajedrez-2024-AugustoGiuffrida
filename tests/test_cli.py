50766db586f010d7ddd092878f86ad74285b09aa
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-09-11 23:32:18 -0300

    Added some cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,17 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from game.cli import Cli
 from game.chess import Chess
 from game.menu import Menu
 
 class TestCli(unittest.TestCase):
+
+    @patch('game.chess.Chess.is_playing', side_effect=[False])
+    def test_start_game_immediately_stops(self, mock_is_playing):
+        cli = Cli()
+        cli.start_game()
+        # Verificar que no se llamó a `play` porque el juego terminó inmediatamente
+        mock_is_playing.assert_called_once()
 
     @patch('builtins.input', side_effect=['1', '1', '2', '2'],)
     @patch('builtins.print')
