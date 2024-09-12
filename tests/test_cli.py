9a3f14e7f1d58c3f40349bb27d172b837c4bad74
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-09-11 22:30:20 -0300

    Added some cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,7 @@ class TestCli(unittest.TestCase):
     @patch('builtins.input', side_effect=['1', '1', '2', '2'],)
     @patch('builtins.print')
 
-    def test_play(self,mock_print,mock_input):
+    def test_play_happy(self,mock_print,mock_input):
         chess = Chess()
         cli = Cli()
         turn = chess.turn
@@ -15,6 +15,18 @@ class TestCli(unittest.TestCase):
         self.assertEqual(turn,"white")
         self.assertEqual(mock_input.call_count,4)
         self.assertEqual(mock_print.call_count,2)
+
+
+    @patch('builtins.input', side_effect=['a','1', '1', '2', '2'])
+    @patch('builtins.print')
+    def test_play_sad(self, mock_print, mock_input):
+        chess = Chess()
+        cli = Cli()
+        cli.play(chess)
+        self.assertEqual(mock_input.call_count, 5)
+        self.assertEqual(mock_print.call_count, 3)
+        mock_print.assert_any_call("Entrada no válida. Por favor, introduce un número entre 0 y 7.")
+        self.assertEqual(chess.turn, "black")
 
 
 if __name__ == '__main__':
