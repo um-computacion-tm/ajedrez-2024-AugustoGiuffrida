01dbe47334c729e90efd11714974d112dbf31612
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2024-10-03 19:18:02 -0300

    Added convert_position  method

--- game/piece.py
+++ game/piece.py
@@ -42,29 +42,48 @@ class Pawn(Pieces):
     white_repr = "♙"
     black_repr = "♟"
 
+class Pawn(Pieces):
+    white_repr = "♙"
+    black_repr = "♟"
+    
     def valid_moves(self, source, dest):
-        # Movimiento hacia adelante
         if self.get_color() == "white":
-            # Mover una casilla hacia adelante
-            if dest[0] - source[0] == 1 and source[1] == dest[1]:
-                return True
-            # Primer movimiento: puede avanzar dos casillas
-            if source[0] == 1 and dest[0] == 3 and source[1] == dest[1]:
-                return True
-            # Captura diagonal
-            if dest[0] - source[0] == 1 and abs(source[1] - dest[1]) == 1:
-                return True
+            return self._valid_white_moves(source, dest)
         elif self.get_color() == "black":
-            # Mover una casilla hacia adelante
-            if source[0] - dest[0] == 1 and source[1] == dest[1]:
-                return True
-            # Primer movimiento: puede avanzar dos casillas
-            if source[0] == 6 and dest[0] == 4 and source[1] == dest[1]:
-                return True
-            # Captura diagonal
-            if source[0] - dest[0] == 1 and abs(source[1] - dest[1]) == 1:
-                return True
+            return self._valid_black_moves(source, dest)
         return False
+    
+    def _valid_white_moves(self, source, dest):
+        if self._move_forward(source, dest, direction=1):  # Movimiento hacia adelante
+            return True
+        if self._initial_double_move(source, dest, direction=1):  # Primer movimiento: puede avanzar dos casillas
+            return True
+        if self._capture_diagonal(source, dest, direction=1):  # Captura diagonal
+            return True
+        return False
+    
+    def _valid_black_moves(self, source, dest):
+        if self._move_forward(source, dest, direction=-1):  # Movimiento hacia adelante
+            return True
+        if self._initial_double_move(source, dest, direction=-1):  # Primer movimiento: puede avanzar dos casillas
+            return True
+        if self._capture_diagonal(source, dest, direction=-1):  # Captura diagonal
+            return True
+        return False
+
+    def _move_forward(self, source, dest, direction):
+        # Mover una casilla hacia adelante
+        return (dest[0] - source[0] == direction) and (source[1] == dest[1])
+
+    def _initial_double_move(self, source, dest, direction):
+        # Primer movimiento: puede avanzar dos casillas
+        start_row = 1 if direction == 1 else 6
+        return (source[0] == start_row) and (dest[0] == source[0] + (2 * direction)) and (source[1] == dest[1])
+
+    def _capture_diagonal(self, source, dest, direction):
+        # Captura diagonal
+        return (dest[0] - source[0] == direction) and (abs(source[1] - dest[1]) == 1)
+
 
 
 
